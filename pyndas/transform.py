8a650c7a6e8e0c572345ecd4148967318925ef46
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-09-26 09:51:34 +0700

    edited - transform

--- pyndas/transform.py
+++ pyndas/transform.py
@@ -1,21 +1,3 @@
-from pymongo import MongoClient
-
-class Koneksi:
-    def __init__(self, uri, db_name):
-        self.client = MongoClient(uri)
-        self.db = self.client[db_name]
-
-    def get_collection(self, collection):
-        return self.db[collection]
-
-    def fetch_data(self,collection_name):
-        collection = self.db[collection_name]
-        return list(collection.find())
-
-    def insert_data(self, collection_name, data):
-        collection = self.get_collection(collection_name)
-        collection.insert_one(data)
-
 class DataTransform:
     def flatten_data(self, data):
         flat_data = {}
